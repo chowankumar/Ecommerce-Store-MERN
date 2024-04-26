7d501fcc57d099fa5a3d25394cb849eaa2f523f1
chowankumar <chowanmalhani5@gmail.com>
2024-04-26 22:32:11 +0500

    Link tags in Navbar.jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import {Link} from "react-router-dom"
 
 
 
@@ -14,15 +15,15 @@ const Navbar = () => {
         <span className='font-semibold text-3xl'>SHOPPER</span>
         </div>
         <ul className='flex gap-12'>
-            <li onClick={()=>{setMenu("shop")}} className='font-semibold active:border-b-2 border-red-300'>Shop{menu == "shop"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
-            <li onClick={()=>{setMenu("men")}} className='font-semibold active:border-b-2 border-red-300'>Men{menu == "men"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
-            <li onClick={()=>{setMenu("women")}} className='font-semibold active:border-b-2 border-red-300'>Women{menu == "women"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}} className='font-semibold active:border-b-2 border-red-300'>Kids{menu == "kids"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}} className='font-semibold active:border-b-2 border-red-300'><Link to='/'>Shop</Link>{menu == "shop"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}} className='font-semibold active:border-b-2 border-red-300'><Link to='/men'>Men</Link>{menu == "men"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}} className='font-semibold active:border-b-2 border-red-300'><Link to='/women'>Women</Link>{menu == "women"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}} className='font-semibold active:border-b-2 border-red-300'><Link to='/kids'>Kids</Link>{menu == "kids"?<hr className='border-none bg-[#FF4141] h-[3px]'/>:<></>}</li>
         </ul>
 
         <div className='flex items-center gap-4'>
-            <button className='border-2 border-gray-300 rounded-full px-6 py-1 hover:bg-gray-50'>Login</button>
-            <img src={cart_icon} alt="" className='w-6'/>
+           <Link to="login">  <button className='border-2 border-gray-300 rounded-full px-6 py-1 hover:bg-gray-50'>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="" className='w-6'/></Link>
             <div className='w-[22px] h-[22px] flex justify-center items-center mt-[-38px] ml-[-28px] text-[14px]  bg-red-600 text-white rounded-[13px]'>0</div>
         </div>
 
