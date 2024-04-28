f6031c84f819df01c5fb6b02145cfb2fcaf431f4
chowankumar <chowanmalhani5@gmail.com>
2024-04-28 20:40:48 +0500

    make offer and popular page responsive

---
+++ frontend/src/Components/Offer/Offer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import exclusive_image from "../Assets/exclusive_image.png"
+
+const Offer = () => {
+  return (
+    <div>
+        <div className='' id='offer-left'> </div>
+        <div className='' id='offer-right'></div>
+    </div>
+  )
+}
+
+export default Offer
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -4,12 +4,12 @@ import Item from '../Item/Item'
 
 const Popular = () => {
   return (
-    <div className=' flex flex-col gap-4 items-center h-screen '>
-       <div className='flex flex-col gap-2'>
+    <div className=' flex flex-col gap-4 items-center h-screen'>
+       <div className='flex flex-col gap-2 mt-72 lg:mt-2'>
        <h1 className='text-4xl font-semibold'>POPULAR IN WOMEN</h1>
         <hr className='w-[150px] h-[4px] bg-black rounded-full'/>
        </div>
-        <div className='flex items-center justify-center gap-3'>
+        <div className='flex items-center justify-center gap-3 flex-wrap'>
             {
                 data_product.map((item,i)=>{
                     return <Item key={i} id={item.id} name={item.name} image={item.image} old_price={item.old_price } new_price={item.new_price}/>
